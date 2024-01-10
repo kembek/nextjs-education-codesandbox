c4312d1e905ff361b20d60fea98fae5338dd5b80
Beknazar Kemelov <zevs.312.777@gmail.com>
2024-01-10 13:40:25 +0600

    adding dynamic routes

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+  return (
+    <>
+      <h1>Product list</h1>
+      <h5>Product 1</h5>
+      <h5>Product 2</h5>
+      <h5>Product 3</h5>
+    </>
+  );
+}
