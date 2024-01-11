c8b25e14fcd42a7321ed7c472202f5ce463cec7f
Beknazar Kemelov <zevs.312.777@gmail.com>
2024-01-11 16:47:15 +0600

    adding examples for understanding and showing file colocation

---
+++ src/app/dashboard/live-chart.tsx
@@ -0,0 +1,8 @@
+export default function LiveChart() {
+  return (
+    <div>
+      <h1>Live chart</h1>
+      <p>This page will not be shown</p>
+    </div>
+  );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+function BarChart() {
+  return (
+    <div>
+      <h1>Bar chart</h1>
+      <p>This component will not be shown</p>
+    </div>
+  );
+}
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>You see some awesome dashboard</p>
+    </div>
+  );
+}
