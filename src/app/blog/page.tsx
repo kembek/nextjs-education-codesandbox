0e0dfb72f3df409cc0a2db2a8d94b4382c8803bf
Beknazar Kemelov <zevs.312.777@gmail.com>
2024-01-12 15:41:15 +0600

    adding loading page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,6 +6,12 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 3000);
+  });
+
   return <h1>Blog</h1>;
 }
