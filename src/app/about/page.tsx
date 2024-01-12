fea72098743003e12f0a0291fc610c29e8d66f92
Beknazar Kemelov <zevs.312.777@gmail.com>
2024-01-12 12:23:37 +0600

    adding metadata in several pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "About page",
+  description: "Created page manually",
+};
+
 export default function About() {
   return <h1>About me</h1>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,19 @@
-export default function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: { productId: string };
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await Promise.resolve(`Product ${params.productId}`);
+
+  return {
+    title,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return <h1>Product details for {params.productId}</h1>;
 }
