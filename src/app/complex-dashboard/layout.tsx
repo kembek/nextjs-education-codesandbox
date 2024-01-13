2668f0d9e7a57fd64059c15a4e2bcd3a6bec379a
Beknazar Kemelov <zevs.312.777@gmail.com>
2024-01-13 14:43:56 +0600

    adding conditional routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -3,13 +3,16 @@ export default function Layout({
   users,
   notifications,
   revenue,
+  login,
 }: {
   children: React.ReactNode;
   notifications: React.ReactNode;
   revenue: React.ReactNode;
   users: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = false;
+  return isLoggedIn ? (
     <div>
       <div>{children}</div>
       <div style={{ display: "flex" }}>
@@ -20,5 +23,7 @@ export default function Layout({
         <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
     </div>
+  ) : (
+    login
   );
 }
